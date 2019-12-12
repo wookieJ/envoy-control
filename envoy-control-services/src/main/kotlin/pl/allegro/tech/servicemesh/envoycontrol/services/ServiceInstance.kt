fbc16635f7f324861523f340288610efaf0524f1
wookieJ <lukaszjedryczka.biuro@gmail.com>
2019-12-12 12:06:52 +0100

    Kotlin bump version

--- envoy-control-services/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/ServiceInstance.kt
+++ envoy-control-services/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/ServiceInstance.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ServiceInstance(
     val id: String,
-    val tags: Set<String>,
     val address: String,
     val port: Int,
+    val tags: Set<String> = emptySet(),
     val regular: Boolean = true,
     val canary: Boolean = false,
     val weight: Int = 1
