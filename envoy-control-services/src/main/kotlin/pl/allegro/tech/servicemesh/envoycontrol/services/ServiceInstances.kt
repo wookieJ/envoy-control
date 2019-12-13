5aa44d8002c038d40109a6d2c502777bbea1e85c
wookieJ <lukaszjedryczka.biuro@gmail.com>
2019-12-13 09:18:39 +0100

    Serialization version 1.14.0

--- envoy-control-services/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/ServiceInstances.kt
+++ envoy-control-services/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/services/ServiceInstances.kt
@@ -1,13 +1,12 @@
 package pl.allegro.tech.servicemesh.envoycontrol.services
 
-import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialId
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ServiceInstances(
     @SerialId(1) val serviceName: String,
-    @SerialId(2) @Optional val instances: Set<ServiceInstance> = emptySet()
+    @SerialId(2) val instances: Set<ServiceInstance> = emptySet()
 ) {
     fun withoutEmptyAddressInstances(): ServiceInstances =
         if (instances.any { it.address.isBlank() }) {
