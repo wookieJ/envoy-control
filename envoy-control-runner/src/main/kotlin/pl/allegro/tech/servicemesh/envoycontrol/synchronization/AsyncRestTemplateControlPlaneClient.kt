f7637e0d5ad621c3b9363d391e290dc8df99c6bc
Marcin Falkowski <marcin.falkowski@allegro.pl>
2019-12-19 15:12:11 +0100

    Measuring time async fix

--- envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/AsyncRestTemplateControlPlaneClient.kt
+++ envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/AsyncRestTemplateControlPlaneClient.kt
@@ -1,14 +1,14 @@
 package pl.allegro.tech.servicemesh.envoycontrol.synchronization
 
-import org.springframework.web.client.AsyncRestTemplate
-import pl.allegro.tech.servicemesh.envoycontrol.services.ServicesState
-import reactor.core.publisher.Mono
-import java.net.URI
 import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Timer
+import org.springframework.web.client.AsyncRestTemplate
 import pl.allegro.tech.servicemesh.envoycontrol.model.ServicesStateProto
 import pl.allegro.tech.servicemesh.envoycontrol.services.ServiceInstance
 import pl.allegro.tech.servicemesh.envoycontrol.services.ServiceInstances
+import pl.allegro.tech.servicemesh.envoycontrol.services.ServicesState
+import reactor.core.publisher.Mono
+import java.net.URI
+import java.util.concurrent.TimeUnit
 
 class AsyncRestTemplateControlPlaneClient(
     val asyncRestTemplate: AsyncRestTemplate,
@@ -17,28 +17,34 @@ class AsyncRestTemplateControlPlaneClient(
 ) : AsyncControlPlaneClient {
 
     override fun getState(uri: URI): Mono<ServicesState> {
-        val sample = Timer.start(meterRegistry)
         val response = asyncRestTemplate.getForEntity<ServicesState>("$uri/state", ServicesState::class.java)
             .completable()
             .thenApply { it.body }
             .let { Mono.fromCompletionStage(it) }
-        sample.stop(meterRegistry.timer("sync-dc-get-state.time"))
+            .elapsed()
+            .map { t ->
+                meterRegistry.timer("sync-dc-get-state.time").record(t.t1, TimeUnit.MILLISECONDS)
+                t.t2
+            }
         return response
     }
 
     override fun getV2State(uri: URI): Mono<ServicesState> {
-        val sample = Timer.start(meterRegistry)
         val response = asyncRestTemplateProto.getForEntity<ServicesStateProto.ServicesState>("$uri/v2/state",
             ServicesStateProto.ServicesState::class.java)
             .completable()
             .thenApply { deserializeProto(it.body) }
             .let { Mono.fromCompletionStage(it) }
-        sample.stop(meterRegistry.timer("sync-dc-get-v2-state.time"))
+            .elapsed()
+            .map { t ->
+                meterRegistry.timer("sync-dc-get-v2-state.time").record(t.t1, TimeUnit.MILLISECONDS)
+                t.t2
+            }
         return response
     }
 
     private fun deserializeProto(body: ServicesStateProto.ServicesState?): ServicesState {
-        val serviceNameToInstances = body?.serviceNameToInstances?.map { entry ->
+        val serviceNameToInstances = body?.serviceNameToInstancesMap?.map { entry ->
             entry.key to ServiceInstances(
                 entry.value.serviceName, entry.value.instancesList.map {
                 ServiceInstance(
