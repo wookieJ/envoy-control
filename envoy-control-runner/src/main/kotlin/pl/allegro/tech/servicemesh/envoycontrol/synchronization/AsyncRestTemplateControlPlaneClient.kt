6b9880f6325d4a2b65137a619fb8671b8b9b5a9d
wookieJ <lukaszjedryczka.biuro@gmail.com>
2019-12-19 10:50:02 +0100

    Measure state endpoint

--- envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/AsyncRestTemplateControlPlaneClient.kt
+++ envoy-control-runner/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/AsyncRestTemplateControlPlaneClient.kt
@@ -4,11 +4,21 @@ import org.springframework.web.client.AsyncRestTemplate
 import pl.allegro.tech.servicemesh.envoycontrol.services.ServicesState
 import reactor.core.publisher.Mono
 import java.net.URI
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 
-class AsyncRestTemplateControlPlaneClient(val asyncRestTemplate: AsyncRestTemplate) : AsyncControlPlaneClient {
-    override fun getState(uri: URI): Mono<ServicesState> =
-        asyncRestTemplate.getForEntity<ServicesState>("$uri/state", ServicesState::class.java)
+class AsyncRestTemplateControlPlaneClient(
+    private val asyncRestTemplate: AsyncRestTemplate,
+    private val meterRegistry: MeterRegistry
+) : AsyncControlPlaneClient {
+
+    override fun getState(uri: URI): Mono<ServicesState> {
+        val sample = Timer.start(meterRegistry)
+        val response = asyncRestTemplate.getForEntity<ServicesState>("$uri/state", ServicesState::class.java)
             .completable()
             .thenApply { it.body }
             .let { Mono.fromCompletionStage(it) }
+        sample.stop(meterRegistry.timer("sync-dc-get-state.time"))
+        return response
+    }
 }
