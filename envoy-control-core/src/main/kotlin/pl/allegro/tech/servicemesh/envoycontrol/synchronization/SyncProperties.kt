0692dd56919a3ae4eace872a29a15de2604d9da6
wookieJ <lukaszjedryczka.biuro@gmail.com>
2020-01-20 09:29:58 +0100

    Cross dc synchronization timeout increased

--- envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/SyncProperties.kt
+++ envoy-control-core/src/main/kotlin/pl/allegro/tech/servicemesh/envoycontrol/synchronization/SyncProperties.kt
@@ -6,8 +6,8 @@ import java.time.Duration
 
 class SyncProperties {
     var enabled = false
-    var pollingInterval: Long = 1
-    var connectionTimeout: Duration = Duration.ofMillis(1000)
-    var readTimeout: Duration = Duration.ofMillis(500)
+    var pollingInterval: Long = 2
+    var connectionTimeout: Duration = Duration.ofMillis(2000)
+    var readTimeout: Duration = Duration.ofMillis(1000)
     var envoyControlAppName = "envoy-control"
 }
